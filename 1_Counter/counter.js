7dc8f6bae207aca57bc4febc1f36e7342a574896
Goga Kot <mesiatsin@icloud.com>
2021-12-05 11:52:06 +0300

    add counter

---
+++ 1_Counter/counter.js
@@ -0,0 +1,14 @@
+alert("Ну здарова уебан, " +
+    "приготовь свои глаза с самому ужасному зрелищу, " +
+    "которое ты только мог себе представить, за то через что я прошел, " +
+    "пока писал эту хуйню, ты поплатишься своими нервными клетками и гандон " +
+    "ты вообще ебаный , я убил на это 6 часов времени . Ебаная ты тварь")
+
+let num = 0;localStorage.getItem("counter");
+function myFunction() {
+    document.getElementById("num").textContent = ++num;
+    localStorage.setItem("counter", document.getElementById("num").textContent);
+
+};
+
+
